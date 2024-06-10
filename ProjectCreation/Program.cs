5ca83ebab1bf35cbe9e85cc913e08b1b9da19f7e
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-10 21:05:45 +0300

    Initial commit

---
+++ ProjectCreation/Program.cs
@@ -0,0 +1,11 @@
+﻿class ProjectCreation
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        int number = int.Parse(Console.ReadLine());
+        int neededHours = number * 3;
+
+        Console.WriteLine($"The architect {name} will need {neededHours} hours to complete {number} project/s.");
+    }
+}
